6766f8d498a860e8651ed5f02a2b81a26d406ed5
saburali <saburali.me@gmail.com>
2023-07-12 22:41:38 +0600

    Page Scroll To Navbar Behaviour Change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,62 +1,82 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light fixed-top">
-    <div class="container">
-      <router-link to="/">
-        <a class="navbar-brand bodyMedium fw-bold themeDarkBG d-inline-block">
-          learnwith <br/> <span class="small-dot">RABBIL</span>
-        </a>
-      </router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li class="nav-item">
-              <router-link to="/">
-                <a class="nav-link text-uppercase active">Home</a>
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/about">
-                <a class="nav-link text-uppercase">About</a>
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/course">
-                <a class="nav-link text-uppercase">Courses</a>
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/mentor">
-                <a class="nav-link text-uppercase">Mentor</a>
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/contact">
-                <a class="nav-link text-uppercase">Contact</a>
-              </router-link>
-            </li>
-            <li class="nav-item">
-              <router-link to="/join">
-                <a class="nav-link text-uppercase rounded-pill text-white themeBG py-2 px-4 ms-md-3">Join</a>
-              </router-link>
-            </li>
-          </ul>
+  <div id="appbar-container" :class="{ mouseScroll: isMouseScroll, removeNavbar: isRemoveNavbar }">
+    <nav class="navbar navbar-expand-lg bg-light fixed-top">
+      <div class="container">
+        <router-link to="/">
+          <a class="navbar-brand bodyMedium fw-bold themeDarkBG d-inline-block">
+            learnwith <br/> <span class="small-dot">RABBIL</span>
+          </a>
+        </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <router-link to="/">
+                  <a class="nav-link text-uppercase active">Home</a>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/about">
+                  <a class="nav-link text-uppercase">About</a>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/course">
+                  <a class="nav-link text-uppercase">Courses</a>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/mentor">
+                  <a class="nav-link text-uppercase">Mentor</a>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/contact">
+                  <a class="nav-link text-uppercase">Contact</a>
+                </router-link>
+              </li>
+              <li class="nav-item">
+                <router-link to="/join">
+                  <a class="nav-link text-uppercase rounded-pill text-white themeBG py-2 px-4 ms-md-3">Join</a>
+                </router-link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HeaderComponent",
-}
+  data() {
+    return {
+      isMouseScroll: false,
+      isRemoveNavbar: false,
+    };
+  },
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      this.isMouseScroll = window.pageYOffset > 700;
+      this.isRemoveNavbar = window.pageYOffset > 100;
+    },
+  },
+};
 </script>
 
 <style scoped>
