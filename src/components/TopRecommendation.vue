7c1295eeeb86b939973f4864ff45b4d895b14d3a
saburali <saburali.me@gmail.com>
2023-07-13 01:33:36 +0600

    Top Recommendation Section - Done

---
+++ src/components/TopRecommendation.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="pt-5 pb-5">
+    <div class="container">
+      <h2 class="section-title mb-4 text-start fw-bold">Top Recomendations</h2>
+      <div class="row">
+        <div class="col-sm-6 col-md-4 col-lg-3 mb-992-25">
+          <div class="card shadow-sm rounded border-0">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="flex-shrink-0">
+                  <div class="border-2 border-success rounded-circle h-60 w-60 d-flex justify-content-center align-items-center">
+                    <span class="border border-5 borderSecondary rounded-circle h-20 w-20 d-inline-block"></span>
+                  </div>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                  <h2 class="headline-4 fw-bold">Mern Stack</h2>
+                  <p class="themeDarkColor mb-0 text-overflow-hidden" style="height: 48px;">This is some content from a media component. This is some content from a media component.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 mb-992-25">
+          <div class="card shadow-sm rounded border-0">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="flex-shrink-0">
+                  <div class="border-2 border-warning rounded-circle h-60 w-60 d-flex justify-content-center align-items-center">
+                    <span class="border border-5 borderSecondary rounded-circle h-20 w-20 d-inline-block"></span>
+                  </div>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                  <h2 class="headline-4 fw-bold">Express REST</h2>
+                  <p class="themeDarkColor mb-0 text-overflow-hidden" style="height: 48px;">This is some content from a media component. This is some content from a media component.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 mb-992-25">
+          <div class="card shadow-sm rounded border-0">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="flex-shrink-0">
+                  <div class="border-2 border-danger rounded-circle h-60 w-60 d-flex justify-content-center align-items-center">
+                    <span class="border border-5 borderSecondary rounded-circle h-20 w-20 d-inline-block"></span>
+                  </div>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                  <h2 class="headline-4 fw-bold">Realtime WEB</h2>
+                  <p class="themeDarkColor mb-0 text-overflow-hidden" style="height: 48px;">This is some content from a media component. This is some content from a media component.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3">
+          <div class="card shadow-sm rounded border-0">
+            <div class="card-body">
+              <div class="d-flex align-items-center">
+                <div class="flex-shrink-0">
+                  <div class="border-2 border-dark rounded-circle h-60 w-60 d-flex justify-content-center align-items-center">
+                    <span class="border border-5 borderSecondary rounded-circle h-20 w-20 d-inline-block"></span>
+                  </div>
+                </div>
+                <div class="flex-grow-1 ms-3">
+                  <h2 class="headline-4 fw-bold">Lumen REST</h2>
+                  <p class="themeDarkColor mb-0 text-overflow-hidden" style="height: 48px;">This is some content from a media component. This is some content from a media component.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopRecomendation',
+};
+</script>
+
+<style scoped>
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,7 @@
     <Header />
     <!-- Main Content -->
     <Hero />
+    <TopRecommendation />
     <Footer />
   </div>
 </template>
@@ -11,10 +12,12 @@
 import Header from '../components/Header';
 import Hero from "@/components/Hero";
 import Footer from '../components/Footer';
+import TopRecommendation from "@/components/TopRecommendation";
 
 export default {
   name: "HomePage",
   components: {
+    TopRecommendation,
     Header,
     Hero,
     Footer
