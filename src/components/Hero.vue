48c1de51ccd2c35cd39cc2a4908d97ba2833adab
saburali <saburali.me@gmail.com>
2023-07-12 22:55:20 +0600

    Hero Section - Done

---
+++ src/components/Hero.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="top-corner-bg lightDeepBG-2 pb-lg-0 pt-lg-0 pt-5 pb-3">
+    <div class="container-fluid my-50">
+      <div class="container">
+        <div class="row flex-wrap-reverse-M992">
+          <div class="col-lg-6 col-12 min-100-vh d-flex align-items-center justify-content-lg-start justify-content-md-center bg-big-shadow min-ha-M992">
+            <div>
+              <h2 class="themeDarkColor fw-bold headline-1 hero-headline-1-res">
+                Learn Confidently with <br /> <span class="text-uppercase themeColor dot">Rabbil Hasan</span>
+              </h2>
+              <p class="themeDarkColor fs-5 my-4 opacity-80">Are you looking for an amazing e-course platform? <br class="d-none d-sm-block" />Don't worry! We got it for you!</p>
+              <div class="position-relative search-courses">
+                <form class="d-flex">
+                  <input type="search" placeholder="Courses Name..." class="me-2 border-0 rounded-pill shadow-lg ps-4 h-65 input-search-pr" aria-label="Search" />
+                  <button class="position-absolute rounded-pill text-white themeBG search-btn" type="submit">Search Courses</button>
+                </form>
+              </div>
+              <div class="row text-center my-4">
+                <div class="col-4">
+                  <h2 class="headline-3 themeDarkColor fw-bold">2617</h2>
+                  <p class="themeDarkColor opacity-80 mb-0">Enrolled Students</p>
+                </div>
+                <div class="col-4">
+                  <h2 class="headline-3 themeDarkColor fw-bold">902</h2>
+                  <p class="themeDarkColor opacity-80 mb-0">Online Course</p>
+                </div>
+                <div class="col-4">
+                  <h2 class="headline-3 themeDarkColor fw-bold">30K</h2>
+                  <p class="themeDarkColor opacity-80 mb-0">Platform Followers</p>
+                </div>
+              </div>
+              <div class="d-flex justify-content-center justify-content-lg-start">
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-info" src="@/assets/images/react-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-success ml--25" src="@/assets/images/vue-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-dark ml--25" src="@/assets/images/js-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-warning ml--25" src="@/assets/images/php-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-dark ml--25" src="@/assets/images/dart-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-primary ml--25" src="@/assets/images/flutter-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-secondary ml--25 d-none d-sm-block" src="@/assets/images/java-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-warning ml--25 d-none d-sm-block" src="@/assets/images/kotlin-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-info ml--25" src="@/assets/images/python-icon.svg" alt="logo" />
+                <img class="themeWhiteBG img-fluid h-60 w-60 p-2 rounded-circle border-2 border-danger ml--25" src="@/assets/images/laravel-icon.svg" alt="logo" />
+                <span class="themeWhiteBG h-60 w-60 p-12 rounded-circle border border-success bw-2 ml--25 d-inline-block headline-4 fw-bold bodyXLarge">20+</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-12 min-100-vh d-flex align-items-center justify-content-lg-start justify-content-md-center min-ha-M992 mb-4 mb-lg-0">
+            <img class="img-fluid position-relative position-right--80 position-right-0-M992" src="@/assets/images/rabbilVai.png" alt="Circle path" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroComponent',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,18 +2,21 @@
   <div>
     <Header />
     <!-- Main Content -->
+    <Hero />
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from '../components/Header';
+import Hero from "@/components/Hero";
 import Footer from '../components/Footer';
 
 export default {
   name: "HomePage",
   components: {
     Header,
+    Hero,
     Footer
   }
 }
