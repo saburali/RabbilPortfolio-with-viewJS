39ae3d2da27dd46f7e549f387c906cde8da64586
saburali <saburali.me@gmail.com>
2023-07-17 11:17:58 +0600

    Contact Page Done

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,12 +1,24 @@
 <template>
-  <div>
-    <h1>Contact Page</h1>
-  </div>
+  <SmallHero
+      dir_name="Home"
+      dir_link="/"
+      current_dir_name="Contact"
+      current_dir_link="contact"
+      title="Have Questions?"
+      name="CONTACT US"
+      description="Are you looking for amazing e-course platform? Don't worry! We got it for you!"
+  />
+  <ContactComponents />
+  <TestimonialComponent />
 </template>
 
 <script>
+import ContactComponents from "@/components/Contact";
+import TestimonialComponent from "@/components/Testimonial";
+import SmallHero from "@/components/SmallHero";
 export default {
-  name: "ContactPage"
+  name: "ContactPage",
+  components: {SmallHero, TestimonialComponent, ContactComponents}
 }
 </script>
 
