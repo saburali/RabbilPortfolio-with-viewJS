d237f9097194ebdf545fd0cc2ace9ed09eba10b1
saburali <saburali.me@gmail.com>
2023-07-17 21:31:27 +0600

    'Small Hero' Component Added to 'CoursesPageOne' Page.

--- src/pages/CoursesPageOne.vue
+++ src/pages/CoursesPageOne.vue
@@ -1,8 +1,23 @@
 <template>
-  <h2> Course Page One </h2>
+  <!-- Small Hero -->
+  <SmallHero
+      dir_name="Home"
+      dir_link="/"
+      current_dir_name="Course Page 01"
+      current_dir_link="courses-page-one"
+      title="Explore our"
+      name="PREMIUM COURSES"
+      description="Are you looking for amazing e-course platform? Don't worry! We got it for you!"
+  />
 </template>
 
 <script>
+import SmallHero from "@/components/SmallHero";
+
+export default {
+  name: "CoursesPageOne",
+  components: {SmallHero}
+}
 </script>
 
 <style scoped>
