8cc3e647bd444a8f45dd0d369ee589f7dc1ddd81
saburali <saburali.me@gmail.com>
2023-07-17 12:49:56 +0600

    "Top Recommendations" Components || Convert Static to Dynamically.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,22 @@
     <Header />
     <!-- Main Content -->
     <Hero />
-    <TopRecommendation />
+
+    <!-- Top Recommendation -->
+    <div class="pt-5 pb-5">
+      <div class="container">
+        <h2 class="section-title mb-4 text-start fw-bold">Top Recommendations</h2>
+        <div class="row">
+          <div class="col-sm-6 col-md-4 col-lg-3 mb-992-25" v-for="item in recommended" :key="item.id">
+            <TopRecommendation
+                :title="item.title"
+                :description="item.description"
+                :border_color="item.b_color"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
     <WhyChooseUs />
     <TopPikedCourse />
     <BrowseCourses />
@@ -33,6 +48,36 @@ export default {
     Hero,
     WhyChooseUs,
     TopPikedCourse,
+  },
+  data() {
+    return {
+      recommended: [
+        {
+          id: 1,
+          title: "MERN Stack",
+          description: "This is some content from a media component. This is some content from a media component.",
+          b_color: "success"
+        },
+        {
+          id: 2,
+          title: "Express REST",
+          description: "This is some content from a media component. This is some content from a media component.",
+          b_color: "danger"
+        },
+        {
+          id: 3,
+          title: "Realtime WEB",
+          description: "This is some content from a media component. This is some content from a media component.",
+          b_color: "warning"
+        },
+        {
+          id: 4,
+          title: "Lumen REST",
+          description: "This is some content from a media component. This is some content from a media component.",
+          b_color: "info"
+        }
+      ]
+    }
   }
 }
 </script>
