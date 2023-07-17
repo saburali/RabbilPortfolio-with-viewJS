088e97487b57472150d07204a218b58b3378edfe
saburali <saburali.me@gmail.com>
2023-07-17 17:11:44 +0600

    "TopPickedCourse.vue" Delete & This Components Covert Static to Dynamic.

--- src/components/OurCoursesCard.vue
+++ src/components/OurCoursesCard.vue
@@ -1,38 +1,46 @@
 <template>
-  <div class="col-6 col-md-6 mb-3 mb-md-4 px-6-mx-5758">
-    <div class="row gx-0 shadow-sm border-bottom-rounded-mx-767">
+  <div class="row gx-0 shadow-sm border-bottom-rounded-mx-767">
       <div class="col-12 col-md-6 lightDeepBG-2 shadow rounded">
-        <img class="img-fluid rounded-start border-top-rounded-mx-767" :src="course_img_1" alt="course_user" />
+        <img class="img-fluid rounded-start border-top-rounded-mx-767" :src="require(`../assets/images/${image}`)" alt="course_user" />
       </div>
       <div class="col-12 col-md-6 pb-1 pt-3 py-md-3 lightDeepBG-2 rounded-end border-bottom-rounded-mx-767">
         <div class="h-100 px-2 px-md-3 position-relative">
           <img class="d-none d-md-block" :src="background" alt="mark-icon" />
-          <span class="d-inline-block position-absolute position-relative-mx-768 bodySmall bodyXSmall-MX-767 position-top-7 position-top-mx-767--13 position-left-30 position-mx-left-0 text-uppercase"> WEB 3.0 </span>
-          <h4 class="bodyLarge fw-bold mb-0 mt-0 mt-md-2 bodySmall-MX-767 position-relative-mx-768 position-top-mx-767--10"> Mern Stack Full Web Application </h4>
+          <span class="d-inline-block position-absolute position-relative-mx-768 bodySmall bodyXSmall-MX-767 position-top-7 position-top-mx-767--13 position-left-30 position-mx-left-0 text-uppercase"> {{ webVersion }} </span>
+          <h4 class="bodyLarge fw-bold mb-0 mt-0 mt-md-2 bodySmall-MX-767 position-relative-mx-768 position-top-mx-767--10"> {{ title }} </h4>
           <div class="position-absolute position-relative-mx-768 position-left-0 position-right-0 position-bottom-0 px-0 px-md-3">
             <div class="d-flex align-items-center">
               <div class="flex-shrink-0">
-                <img class="img-fluid rounded-circle border border-success h-30-mx-768" :src="user_avatar" alt="course_user" />
+                <img class="img-fluid rounded-circle border border-success h-30-mx-768" :src="require(`../assets/images/${authorImg}`)" alt="course_user" />
               </div>
               <div class="flex-grow-1 ms-2 ms-md-3 my-2 py-md-4">
-                <h4 class="bodyMedium fw-bold m-0 bodyXSmall-MX-767"> Rabbil Hasan </h4>
+                <h4 class="bodyMedium fw-bold m-0 bodyXSmall-MX-767"> {{ name }} </h4>
               </div>
             </div>
             <hr class="my-2 my-md-0" />
             <div class="d-flex justify-content-between bodyXSmall-MX-767">
-              <span class="text-uppercase themeColor fw-bold"> BDT. 5000 </span>
-              <span> 5 (120) </span>
+              <span class="text-uppercase themeColor fw-bold"> BDT. {{ price }} </span>
+              <span> {{ review }} ({{ reviewCount }}) </span>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
   name: 'OurCoursesCard',
+  props: {
+    title: String,
+    image: String,
+    name: String,
+    price: String,
+    review: String,
+    reviewCount: String,
+    authorImg: String,
+    webVersion: String,
+  },
   data() {
     return {
       course_img_1: require('../assets/images/course-Image1.png'),
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -20,7 +20,31 @@
       </div>
     </div>
     <WhyChooseUs />
-    <TopPikedCourse />
+
+    <!-- Top Picked Course -->
+    <div class="pt-5 pb-5">
+      <div class="container position-relative">
+        <p class="small-section-title fw-bold themeColor mb-2 headline-2"> Our Course </p>
+        <div class="d-flex justify-content-center">
+          <h2 class="section-title fw-bold mb-4"> Our Top-Picked Courses </h2>
+        </div>
+        <div class="row coursesBG">
+          <div class="col-6 col-md-6 mb-3 mb-md-4 px-6-mx-5758" v-for="item in pickedCourse" :key="item.id">
+            <OurCoursesCard
+                :title="item.title"
+                :image="item.image"
+                :name="item.name"
+                :price="item.price"
+                :review="item.review"
+                :reviewCount="item.reviewCount"
+                :authorImg="item.authorImg"
+                :webVersion="item.webVersion"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
     <BrowseCourses />
     <Testimonial />
     <Contact />
@@ -32,14 +56,15 @@ import Header from '../components/Header';
 import Hero from "@/components/Hero";
 import TopRecommendation from "@/components/TopRecommendation";
 import WhyChooseUs from "@/components/WhyChooseUs";
-import TopPikedCourse from "@/components/TopPickedCourse";
 import BrowseCourses from "@/components/BrowseCourse";
 import Testimonial from "@/components/Testimonial";
 import Contact from "@/components/Contact";
+import OurCoursesCard from "@/components/OurCoursesCard";
 
 export default {
   name: "HomePage",
   components: {
+    OurCoursesCard,
     Contact,
     Testimonial,
     BrowseCourses,
@@ -47,7 +72,6 @@ export default {
     Header,
     Hero,
     WhyChooseUs,
-    TopPikedCourse,
   },
   data() {
     return {
@@ -76,6 +100,73 @@ export default {
           description: "This is some content from a media component. This is some content from a media component.",
           b_color: "info"
         }
+      ],
+      pickedCourse: [
+        {
+          id: 1,
+          title: "Mern Stack Full Web Application",
+          image: "course-Image1.png",
+          name: "Rabbil Hasan",
+          authorImg: "rabbil-vai-avater.png",
+          price: "4000",
+          review: "4.5",
+          reviewCount: "45",
+          webVersion: "WEB 3.0",
+        },
+        {
+          id: 2,
+          title: "Mern Stack Full Web Application",
+          image: "course-Image1.png",
+          name: "Rabbil Hasan",
+          authorImg: "rabbil-vai-avater.png",
+          price: "4000",
+          review: "4.5",
+          reviewCount: "45",
+          webVersion: "WEB 3.0",
+        },
+        {
+          id: 3,
+          title: "Mern Stack Full Web Application",
+          image: "course-Image1.png",
+          name: "Rabbil Hasan",
+          authorImg: "rabbil-vai-avater.png",
+          price: "4000",
+          review: "4.5",
+          reviewCount: "45",
+          webVersion: "WEB 3.0",
+        },
+        {
+          id: 4,
+          title: "Mern Stack Full Web Application",
+          image: "course-Image1.png",
+          name: "Rabbil Hasan",
+          authorImg: "rabbil-vai-avater.png",
+          price: "4000",
+          review: "4.5",
+          reviewCount: "45",
+          webVersion: "WEB 3.0",
+        },{
+          id: 5,
+          title: "Mern Stack Full Web Application",
+          image: "course-Image1.png",
+          name: "Rabbil Hasan",
+          authorImg: "rabbil-vai-avater.png",
+          price: "4000",
+          review: "4.5",
+          reviewCount: "45",
+          webVersion: "WEB 3.0",
+        },
+        {
+          id: 6,
+          title: "Mern Stack Full Web Application",
+          image: "course-Image1.png",
+          name: "Rabbil Hasan",
+          authorImg: "rabbil-vai-avater.png",
+          price: "4000",
+          review: "4.5",
+          reviewCount: "45",
+          webVersion: "WEB 3.0",
+        },
       ]
     }
   }
